23fe2c9eb2bf2d4d4cd322eeb3ea0d3e541daab4
Ken Tanaka Hernández <ktanaka@ictp.it>
2024-01-27 09:14:12 +0100

    PCA set up

--- src/pinesClass.py
+++ src/pinesClass.py
@@ -36,6 +36,8 @@ DATASET_STR = "Dataset"
 DATASET_PATH = os.path.join(DATA_PATH, "Indian_pines_corrected.mat")
 DATASET_NAME = "indian_pines_corrected"
 
+myDPI = 96
+
 def restricted_float(x):
   try:
     x = float(x)
@@ -49,11 +51,17 @@ def restricted_float(x):
 parser = argparse.ArgumentParser(prog="Classify the pines based on Bands")
 parser.add_argument("-f", "--force", action='store_true', default=False,
                     required=False,
-                    help="Forces to compute everything, without considering"
-                         "previuos computed results")
+                    help="Forces to compute everything, without considering "
+                         "previuos computed results. Default: False")
 parser.add_argument("-t", "--test", type=restricted_float, default=0.3,
-                    required=False,
-                    help="")
+                    required=False, metavar="[0, 1]",
+                    help="Size ratio between Testing and Training Datasets. "
+                         "If this ratio is set to zero, then the program will "
+                         "Train in the entire Dataset. Default: 0.3")
+parser.add_argument("--pca", type=int, required=False, default=42,
+                    choices=range(0, 201), metavar="[0-200]",
+                    help="Enable Principal Component Analysis (PCA) with n "
+                         "components. Default: 42")
 
 # Initializing Classifiers
 #clf1 = LogisticRegression(random_state=1, solver='lbfgs')
@@ -62,7 +70,7 @@ parser.add_argument("-t", "--test", type=restricted_float, default=0.3,
 #clf3 = GaussianNB()
 #clf4 = SVC(gamma='auto')
 
-def plt_im(y, path, name, cmap="nipy_spectral"):
+def plt_im(y, path, name, cmap = "nipy_spectral"):
   plt.figure(figsize=(10, 8))
   plt.imshow(y, cmap=cmap)
   plt.colorbar()
@@ -71,14 +79,55 @@ def plt_im(y, path, name, cmap="nipy_spectral"):
   plt.savefig(os.path.join(path, name + ".png"))
   return
 
-def sns_im(y, path, name, cmap="coolwarm"):
-  plt.figure(figsize=(15, 10))
+def sns_im(y, path:str, name:str, cmap:str = "coolwarm"):
+  plt.figure(figsize=(1000/myDPI, 800/myDPI), dpi=myDPI)
   sns.heatmap(y, cmap=cmap, annot=False)
+  plt.title(name)
+  plt.savefig(os.path.join(path, name + ".png"), dpi=myDPI*10)
+  return
+
+def plt_attr(ylabel:str = None, xlabel:str = None,
+             yscale: str = None, xscale: str = None):
+  if xlabel is not None:
+    plt.xlabel(xlabel)
+  if xscale is not None:
+    plt.xscale(xscale)
+  if yscale is not None:
+    plt.yscale(yscale)
+  if ylabel is not None:
+    plt.ylabel(ylabel)
+  return
+
+# TODO: Use kwargs
+def plt_pl(y, path:str, name:str, color:str = "blue", x = None,
+           ylabel:str = None, xlabel:str = None, yscale: str = None,
+           xscale: str = None):
+  plt.figure(figsize=(12, 6))
+  if x is None:
+    plt.plot(y, marker='o', linewidth=2, color=color)
+  else:
+    plt.plot(x, y, marker='o', linewidth=2, color=color)
+  plt_attr(ylabel, xlabel, yscale, xscale)
   plt.title(name)
   plt.savefig(os.path.join(path, name + ".png"))
   return
 
-def plt_pl():
+# TODO: Use kwargs
+def plt_sc(x, y, path:str, name:str, ylabel:str = None, xlabel:str = None,
+           yscale: str = None, xscale: str = None, c = None, z = None,
+           zlabel = None):
+  if z is None:
+    plt.scatter(x, y, c=c)
+    plt_attr(ylabel, xlabel, yscale, xscale)
+    plt.title(name)
+  else:
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(x, y, z, c=c)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+  plt.savefig(os.path.join(path, name + ".png"))
   return
 
 def main(args):
@@ -86,6 +135,7 @@ def main(args):
   # Exploratory Data Analysis (EDA)
   SECTION = "EDA"
   if args.force or not os.path.isfile(myDataSet):
+    # TODO: Remove all '.pkl' & '.json' files
     from scipy.io import loadmat
     # Load Dataset and Truth
     X = loadmat(DATASET_PATH)[DATASET_NAME]
@@ -102,6 +152,9 @@ def main(args):
           os.path.join(IMG_PATH, SECTION), TARGET_STR)
   else:
     X = pd.read_pickle(myDataSet)
+    with open(os.path.join(DATA_PATH, "Datashape.json"), 'r') as fp:
+      myDict = json.load(fp)
+      X_SHAPE = [myDict[x] for x in ['X', 'Y', 'Z']]
     FEATURES = X.columns.values[:-1]
 
   scaler = StandardScaler()
@@ -125,13 +178,32 @@ def main(args):
     X_train = X[FEATURES]
     y_train = X[TARGET_STR]
 
-  sns_im(X_train.corr(), os.path.join(IMG_PATH, SECTION), "Correlation_Mtx")
+  sns_im(X.corr(), os.path.join(IMG_PATH, SECTION), "Correlation_Mtx")
+  FEATURES_RELEVANT = np.abs(X.corr()[TARGET_STR]) > 0.5
+  if args.pca != 0:
+    # Principal Component Analysis (PCA)
+    name = f"PCA_{args.pca:03}"
+    myDataSet = os.path.join(DATA_PATH, DATASET_STR + name + ".pkl")
+    if args.force or not os.path.isfile(myDataSet):
+      pinesPCA = PCA(n_components=args.pca, svd_solver="full")
+      pinesPCA.set_output(transform="pandas")
+      XpinesPCA = pinesPCA.fit_transform(X_train)
+      XpinesPCA.to_pickle(myDataSet)
+      PC1, PC2, PC3 = XpinesPCA[
+                        pinesPCA.get_feature_names_out()[:3]].to_numpy().T
+      plt_pl(pinesPCA.explained_variance_ratio_,
+             os.path.join(IMG_PATH, SECTION), name + "_VarExp", yscale="log",
+             xlabel="Principal Components", ylabel="Variance Explained")
+      plt_sc(PC1, PC2, os.path.join(IMG_PATH, SECTION),
+             name + "_" + TARGET_STR, c=y_train, z=PC3,
+             xlabel="Principal Component 1",
+             ylabel="Principal Component 2",
+             zlabel="Principal Component 3")
+    else:
+      XpinesPCA = pd.read_pickle(myDataSet)
 
-  # Principal Component Analysis (PCA)
-  pinesPCA = PCA(svd_solver="full")
-  pinesPCA.set_output(transform="pandas")
-  XpinesPCA = pinesPCA.fit_transform(X_train)
-  print(XpinesPCA)
+  else:
+    pass
 
   # Linear Discrimination Analysis (LDA)
   pinesLDA = LinearDiscriminantAnalysis().set_output(transform="pandas")
