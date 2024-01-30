a8db37b7880db3054720ff9c1f784f6fb32d4be2
ktanakah <kentanaka3@outlook.com>
2024-01-30 11:38:51 +0100

    Add run.sh

---
+++ src/run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+#SBATCH --job-name="Python MM"
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH  -A ICT23_MHPC
+#SBATCH --time 1:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=49000MB
+#SBATCH  -p boost_usr_prod
+#SBATCH -e %j.err
+#SBATCH -o %j.out
