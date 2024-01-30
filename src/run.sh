4131f9d9eabd5f566a4385ea3bd991fb176e52e0
acarducc <acarducc@login05.leonardo.local>
2024-01-30 23:11:05 +0100

    added results from PCA only and LDA only, for RF, SVC and LogR

--- src/run.sh
+++ src/run.sh
@@ -5,11 +5,11 @@
 #SBATCH --tasks-per-node=32
 #SBATCH --cpus-per-task=1
 #SBATCH  -A ICT23_MHPC
-#SBATCH --time 2:00:00
+#SBATCH --time 0:10:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=49000MB
 #SBATCH  -p boost_usr_prod
 #SBATCH -e %j.err
 #SBATCH -o %j.out
 
-python pinesClass.py --lda 12 --RF 20 -f > RF_20_LDA_12
+python pinesClass.py --pca 20 --SVC > SVC_PCA_20
